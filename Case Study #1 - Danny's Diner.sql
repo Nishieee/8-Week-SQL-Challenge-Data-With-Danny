a47aa8c6a4f73cfc2cab21d8be98a258115b4703
NOISHEE <matlaninishi@gmail.com>
2024-09-06 21:07:55 -0400

    Update Case Study #1 - Danny's Diner.sql

--- Case Study #1 - Danny's Diner.sql
+++ Case Study #1 - Danny's Diner.sql
@@ -31,5 +31,82 @@ limit 1;
 
 -- Q5 
 
+with customer_count as(
+select s.customer_id, s.product_id, m.product_name, count(s.product_id) as purchased_item 
+from sales s 
+left join menu m 
+on s.product_id = m.product_id
+group by s.customer_id, s.product_id, m.product_name), 
+ranked_items as(
+select customer_id, product_id, product_name, purchased_item, row_number() over (partition by customer_id order by purchased_item desc) as rank 
+from customer_count)
+
+select customer_id, product_name
+from ranked_items
+where rank = 1
+
+
+
+-- Q6 
+
+with cte as (
+SELECT s.customer_id, s.product_id, s.order_date, m.join_date, row_number() over (partition by s.customer_id order by s.order_date) as ranked_columns
+from sales s 
+join members m 
+on s.customer_id = m.customer_id
+WHERE s.order_date > m.join_date)
+
+select me.product_name, c.customer_id
+from cte c 
+join menu me 
+on c.product_id = me.product_id
+WHERE c.ranked_columns = 1;
+
+
+
+-- Q7
+
+with cte as (
+SELECT s.customer_id, s.product_id, s.order_date, m.join_date, rank() over (partition by s.customer_id order by s.order_date desc) as ranked_columns
+from sales s 
+join members m 
+on s.customer_id = m.customer_id
+WHERE s.order_date < m.join_date)
+
+select me.product_name, c.customer_id
+from cte c 
+join menu me 
+on c.product_id = me.product_id
+WHERE c.ranked_columns = 1;
+
+
+-- Q8 
+
+WITH cte AS (
+  SELECT s.customer_id, 
+         s.product_id, 
+         COUNT(s.product_id) AS product_count, 
+         s.order_date
+  FROM sales s 
+  JOIN members m 
+  ON s.customer_id = m.customer_id
+  WHERE s.order_date < m.join_date 
+  GROUP BY s.customer_id, s.product_id, s.order_date
+)
+
+SELECT c.customer_id, 
+       SUM(c.product_count) AS total_items,  
+       SUM(me.price * c.product_count) AS total_spent 
+FROM cte c 
+JOIN menu me 
+ON c.product_id = me.product_id
+GROUP BY c.customer_id;
+
+
+
+
+ 
+
+
 
 
